384f3f237f9a4822c3c27ec174366528aeb9aa96
Charles S <chuckiels2011@gmail.com>
2014-03-06 17:20:59 -0500

    Maybe fixed?

--- WordCleaner.js
+++ WordCleaner.js
@@ -4,64 +4,63 @@
 //
 // Please excuse the swear words array...
 
-var WordCleaner = {
-  this.wordList /*AKA the bad word table*/ = ["fuck", "shit", "cunt", "bitch", "nigger", "asshole", "bullshit", "damn", "slut"];
-  function getStars(word)
+wordList /*AKA the bad word table*/ = ["fuck", "shit", "cunt", "bitch", "nigger", "asshole", "bullshit", "damn", "slut"];
+function getStars(word)
+{
+  var str = "";
+  for(var i = 0; i < word.length; i++)
   {
-    var str = "";
-    for(var i = 0; i < word.length; i++)
-    {
+    str += "*";
+  }
+  return str;
+}
+
+function getStarsAndFirstLetter(word)
+{
+  var str = word[0];
+  for(var i = 1; i < word.length; i++)
+  {
+    str += "*";
+  }
+  return str;
+}
+
+function comicReplace(word)
+{
+  str = "";
+  for(var i = 0; i < word.length; i++)
+  {
+    if(word[i] == "s")
+      str += "$";
+    else if(word[i] == "a")
+      str += "%";
+    else if(word[i] == "e" || word[i] == "i" || word[i] == "o" || word[i] == "u" || word[i] == "y")
       str += "*";
-    }
-    return str;
+    else
+      str += "#";
   }
+  return str;
+}
 
-  function getStarsAndFirstLetter(word)
+function cleanWords(wordList, convertFunc)
+{
+  for(var i = 0; i < wordList.length; i++) // Checks each word in the wordlist
   {
-    var str = word[0];
-    for(var i = 1; i < word.length; i++)
-    {
-      str += "*";
-    }
-    return str;
-  }
-
-  function comicReplace(word)
-  {
-    str = "";
-    for(var i = 0; i < word.length; i++)
-    {
-      if(word[i] == "s")
-        str += "$";
-      else if(word[i] == "a")
-        str += "%";
-      else if(word[i] == "e" || word[i] == "i" || word[i] == "o" || word[i] == "u" || word[i] == "y")
-        str += "*";
-      else
-        str += "#";
-    }
-    return str;
-  }
-
-  function cleanWords(this.wordList, convertFunc)
-  {
-    for(var i = 0; i < this.wordList.length; i++) // Checks each word in the wordlist
-    {
-      var word = this.wordList[i];
-      word = word.toLowerCase();
-      var start = 0;
-      var end = 0;
-      $('p, a, h1, h2, h3, h4, h5, h6, span, div, b, i, s, u, block, quote, code').contents().filter(
+    var word = wordList[i];
+    word = word.toLowerCase();
+    var start = 0;
+    var end = 0;
+    $('p, a, h1, h2, h3, h4, h5, h6, span, div, b, i, s, u, blockquote, code').contents().filter(
+        function() {
+          return this.nodeType == Node.TEXT_NODE && this.nodeValue.indexOf(word) >= 0;
+        }).each(
           function() {
-            return this.nodeType == Node.TEXT_NODE && this.nodeValue.indexOf(word) >= 0;
-          }).each(
-            function() {
-              console.log(this.nodeValue);
-              var re = new RegExp(word + "(ing|er|ed|y|ty|)(s|)","ig");
-              this.nodeValue = this.nodeValue.replace(re, convertFunc(word));
-            });
-    }
+            console.log(this.nodeValue);
+            var re = new RegExp(word + "(ing|er|ed|y|ty|)(s|)","ig");
+            this.nodeValue = this.nodeValue.replace(re, convertFunc(word));
+          });
   }
 }
 
-$( document ).ready(WordCleaner.cleanWords(WordCleaner.wordList, function(word){return WordCleaner.getStars(word)}))
+
+$( document ).ready(cleanWords(wordList, function(word){return getStars(word)}))
